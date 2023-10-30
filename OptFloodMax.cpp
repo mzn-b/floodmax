3d8d41e4fdc2ec3b364d9b16d9763f08674a5890
marco <marco.ross.vienna@gmail.com>
2023-10-30 13:41:21 +0100

    Using async receives with a timeout of 1 second in OptFloodMax to terminate rounds.

--- OptFloodMax.cpp
+++ OptFloodMax.cpp
@@ -2,42 +2,54 @@
 #include <vector>
 #include <algorithm>
 #include <mpi.h>
+#include <unistd.h>
 
 int main(int argc, char** argv) {
-    MPI_Init(&argc, &argv);
-
     int world_size, rank;
+
+    MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     int diam = world_size; // In a real-world application, this might not be equal to world_size
-    int u = rank;
-    int max_uid = u;
+    int max_uid = rank;
     int rounds = 0;
     bool new_info = true;
 
-    while (rounds < diam) {
-        std::vector<int> received_uids;
 
-        // Sending phase
+    while (rounds < diam) {
+        std::cout << "Process " << rank << " entering round " << rounds << std::endl;
+        // std::cout << "Sending from " << rank << " in round " << rounds << std::endl;
+        // Sending phase>
         if (new_info) {
             for (int i = 0; i < world_size; i++) {
                 if (i != rank) {
-                    MPI_Send(&max_uid, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+                    MPI_Send(&max_uid, 1, MPI_INT, i, rounds, MPI_COMM_WORLD);
                 }
             }
         }
 
+        // std::cout << "Receiving at " << rank << " in round " << rounds << std::endl;
         // Receiving phase
+        MPI_Request r[world_size-1];
+        MPI_Status statuses[world_size-1];
+        int recv_uids[world_size-1];
         for (int i = 0; i < world_size - 1; i++) {  // We expect to receive world_size-1 messages (everyone except ourselves)
-            int recv_uid;
-            MPI_Recv(&recv_uid, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            received_uids.push_back(recv_uid);
+            recv_uids[i] = -1;
+            MPI_Irecv(&recv_uids[i], 1, MPI_INT, MPI_ANY_SOURCE, rounds, MPI_COMM_WORLD, &r[i]);
+        }
+
+        sleep(1);
+        int received[world_size-1];
+        for (int i = 0; i < world_size - 1; i++) {
+            MPI_Test(&r[i], &received[i], &statuses[i]);
+            if (!received[i]) MPI_Cancel(&r[i]);
         }
 
         // Update max_uid
         int old_max_uid = max_uid;
-        max_uid = std::max(max_uid, *std::max_element(received_uids.begin(), received_uids.end()));
+        max_uid = std::max(max_uid, *std::max_element(recv_uids, recv_uids+world_size-1));
+        // std::cout << "Receiving max_uid as " << max_uid << " at process " << rank << std::endl;
 
         // Update new_info flag
         new_info = old_max_uid != max_uid;
@@ -45,8 +57,9 @@ int main(int argc, char** argv) {
         rounds++;
     }
 
+
     // Determine leader status
-    if (max_uid == u) {
+    if (max_uid == rank) {
         std::cout << "Process " << rank << " is the leader!" << std::endl;
     } else {
         std::cout << "Process " << rank << " is not the leader." << std::endl;
